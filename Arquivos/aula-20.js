2474b6d704671617b3c2c54b03550bc5023922ee
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2024-02-17 16:11:06 -0300

    aula-20

---
+++ Arquivos/aula-20.js
@@ -0,0 +1,12 @@
+function mudartexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="CFB Curso"
+    d2.innerHTML="CFB Curso"
+    d3.innerHTML="CFB Curso"
+    
+    
+}
+
+
