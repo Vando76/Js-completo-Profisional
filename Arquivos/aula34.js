6c99ac6ef2d9d7ad52a7710e2ffb18900be33131
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2024-03-04 20:00:21 -0300

    aula34

---
+++ Arquivos/aula34.js
@@ -0,0 +1,26 @@
+//const c1=document.getElementById("c1")
+//const c1=document.querySelector("#c1")
+const cursos=[...document.querySelectorAll(".curso")]
+
+cursos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        const el=evt.target
+    el.classList.add("destaque")
+    console.log(el.innerHTML + " foi clicado")
+
+
+    })
+})
+
+//const msg=()=>{
+//    alert('Clicou')
+//}
+
+//c1.addEventListener("click",(evt)=>{
+
+//    const el=evt.target
+//    el.classList.add("destaque")
+    
+//})
+
+
