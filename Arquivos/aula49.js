369d8811397513106f6183ab1779f4290c44a0c5
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2024-04-03 20:10:43 -0300

    aula48 e 49

--- Arquivos/aula49.js
+++ Arquivos/aula49.js
@@ -2,12 +2,12 @@ const p_array=document.querySelector("#array")
 const btnVerificar=document.querySelector("#btnVerificar")
 const resultado=document.querySelector("#resultado")
 
-const elementos_array=[21,16,19,20,16,18,22]
+const elementos_array=[16,12,20,10,17,15,13,11,19]
 
 p_array.innerHTML="{"+elementos_array+"}"
 
 btnVerificar.addEventListener("click",(evt)=>{
-    const ret=elementos_array.every((e,i)=>{
+    const ret=elementos_array.some((e,i)=>{
         if(e<18){
             resultado.innerHTML="Array não conforme na posiçao do " +i
 
