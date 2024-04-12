a0c4035d5fecab7088b74faf24b98beacc367358
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2024-04-12 20:16:56 -0300

    aula54

---
+++ Arquivos/aula54.js
@@ -0,0 +1,10 @@
+const caixa=document.querySelector("#caixa")
+
+let mapa=new Map()
+
+mapa.set("curso","JavaScript")
+mapa.set(10,"CFB Cursos")
+mapa.set(1,100)
+mapa.set("canal",100)
+
+console.log(mapa)
