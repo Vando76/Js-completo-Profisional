42dc4bc0e2a1c6c82c06c95906e4854a00470927
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2024-04-13 16:47:10 -0300

    aula54

--- Arquivos/aula54.js
+++ Arquivos/aula54.js
@@ -7,4 +7,22 @@ mapa.set(10,"CFB Cursos")
 mapa.set(1,100)
 mapa.set("canal",100)
 
+mapa.delete(1)
+
 console.log(mapa)
+
+let pes=10
+let res=""
+if(mapa.has(pes)){
+    res="A chave existe na coleção com o valor: " + mapa.get(pes)
+    
+}else{
+    res="A chave Não está na coleção" 
+}
+res+="<br/> O tamanho da coleçao é " + mapa.size
+
+caixa.innerHTML=res
+
+mapa.forEach((el)=>{
+    console.log(el)
+})
