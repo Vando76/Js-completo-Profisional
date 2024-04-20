59f3b36224e4ce8055dab24f1b8eb7c7a3cd82d3
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2024-04-20 18:19:58 -0300

    aula58

---
+++ Arquivos/aula58.js
@@ -0,0 +1,14 @@
+class Pessoa{
+    constructor(pnome){
+        this.nome=pnome
+    }
+
+}
+
+let p1=new Pessoa("Vando")
+let p2=new Pessoa("Willian")
+let p3=new Pessoa("Rogério")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
