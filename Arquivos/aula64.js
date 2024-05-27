e55177863170f40aa926b9af0e1ce364402bd68c
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2024-05-27 16:21:20 -0300

    aula64

---
+++ Arquivos/aula64.js
@@ -0,0 +1,11 @@
+const pessoa={
+    nome:"Vando",
+    canal:"CFB Cursos",
+    curso:"Javascript",
+    aulas:{
+        aula1:"Intodução",
+        aula2:"Variáveis",
+        aula3:"Condicional"
+    }
+
+}
